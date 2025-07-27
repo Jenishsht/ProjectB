816c02adfb3b02d5cf9103aa43dd4167b13ec9ad
Jenish Shrestha <shtjenish@gmail.com>
2025-07-27 14:08:31 +0545

    Initial commit

---
+++ Controllers/MemberController.cs
@@ -0,0 +1,29 @@
+using System;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[Route("api/[controller]")]   //localhost:5001/api/member
+[ApiController]
+public class MemberController(AppDbContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task< ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+    {
+        var members = await context.Users.ToListAsync();
+        return members;
+
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>>GetMember(string id)
+    {
+        var member = await context.Users.FindAsync(id);
+        if (member == null) return NotFound();
+        return member;
+    }
+
+
+}
