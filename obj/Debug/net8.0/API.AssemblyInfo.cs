3a194571538b55d74394202c73e0385e3cc47ac5
Jenish Shrestha <shtjenish@gmail.com>
2025-11-02 16:33:28 +0545

    angular-error handling

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0436486b8d93eb96dce0f756cd61c87b434fb8cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0000f684d1a1e214516356f64e83f6829f4a9c71")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
